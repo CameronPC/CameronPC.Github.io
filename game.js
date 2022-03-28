779fc6bef3389a4978b66d7e82b3787f5c5966a5
Brady002 <bradywright@u.boisestate.edu>
2022-03-28 16:09:50 -0600

    GAME

--- game.js
+++ game.js
@@ -7,10 +7,14 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 const text = document.querySelector('.gametext')
 
+const reset = document.getElementById("resetbutton")
+const start = document.getElementById("startbutton")
+
 let result = 0
 let hitPosition
 let currentTime = 60
 let timerId = null
+let gameStart = false;
 
 let images = ['bradygameimg',
   'camgameimg',
@@ -21,58 +25,102 @@ let deathMessages = ['Brady has a book series', 'Cam is a pog dungeon master',
   'Ryker has a twitch.tv channel', 'Myles is also a dungeon master'];
 let randomPhoto = images[Math.floor(Math.random() * images.length)]
 
-function randomSquare() {
-  randomPhoto = images[Math.floor(Math.random() * images.length)]
-
-  squares.forEach(square => {
-    square.innerHTML = ''
-    images.forEach(photo => {
-      square.classList.remove(photo)
-
-    })
-  })
-
-  let randomSquare = squares[Math.floor(Math.random() * 9)]
-  randomSquare.classList.add(randomPhoto)
-
-  hitPosition = randomSquare.id
-
-}
-
-squares.forEach(square => {
-  square.addEventListener('mousedown', () => {
-    if (square.id == hitPosition) {
-      result++
-      score.textContent = result
-      hitPosition = null
-      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      document.getElementById("game-foreground").style.backgroundColor = "#" + randomColor;
-      square.classList.remove(randomPhoto)
-      let messageToPhoto = images.indexOf(randomPhoto)
-      square.innerHTML = deathMessages[messageToPhoto]
+start.addEventListener('click', () => {
+    if (gameStart != true) {
+        console.log("started!")
+        gameStart = true
+        beginGame()
     }
-
-  })
 })
 
+reset.addEventListener('click', () => {
+    gameStart = false
+    currentTime = 60
+    timeLeft.textContent = currentTime
+    result = 0;
+    score.textContent = result
+    document.getElementById("game-foreground").style.backgroundColor = "#242424"
+    squares.forEach(square => {
+        square.innerHTML = ''
+        images.forEach(photo => {
+            square.classList.remove(photo)
+
+        })
+    })
+
+    beginGame()
+})
+
+function beginGame() {
+    if (gameStart == true) {
+        function randomSquare() {
+            if (gameStart == true) {
+                randomPhoto = images[Math.floor(Math.random() * images.length)]
+
+                squares.forEach(square => {
+                    square.innerHTML = ''
+                    images.forEach(photo => {
+                        square.classList.remove(photo)
+
+                    })
+                })
+
+                let randomSquare = squares[Math.floor(Math.random() * 9)]
+                randomSquare.classList.add(randomPhoto)
+
+                hitPosition = randomSquare.id
+            }
+        }
+
+        if (gameStart == true) {
+            squares.forEach(square => {
+                square.addEventListener('mousedown', () => {
+                    if (square.id == hitPosition) {
+                        result++
+                        score.textContent = result
+                        hitPosition = null
+                        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                        document.getElementById("game-foreground").style.backgroundColor = "#" + randomColor;
+                        square.classList.remove(randomPhoto)
+                        let messageToPhoto = images.indexOf(randomPhoto)
+                        square.innerHTML = deathMessages[messageToPhoto]
+                    }
+
+                })
+            })
+        }
+        
 
 
-function moveMole() {
-  timerId = setInterval(randomSquare, 1000)
+        if (gameStart == true) {
+            function moveMole() {
+                timerId = setInterval(randomSquare, 1000)
+            }
+
+            moveMole()
+        }
+        
+
+        
+
+        function countDown() {
+            if (gameStart == true) {
+                currentTime--
+                timeLeft.textContent = currentTime
+
+                if (currentTime == 0) {
+                    clearInterval(countDownTimerId)
+                    clearInterval(timerId)
+                    alert('GAME OVER! Your final score is ' + result)
+                    result = 0
+                    score.textContent = result
+                    gameStart = false;
+                }
+            }
+        }
+
+        let countDownTimerId = setInterval(countDown, 1000)
+    }
 }
+ 
 
-moveMole()
-
-function countDown() {
-  currentTime--
-  timeLeft.textContent = currentTime
-
-  if (currentTime == 0) {
-    clearInterval(countDownTimerId)
-    clearInterval(timerId)
-    alert('GAME OVER! Your final score is ' + result)
-  }
-
-}
-
-let countDownTimerId = setInterval(countDown, 1000)
