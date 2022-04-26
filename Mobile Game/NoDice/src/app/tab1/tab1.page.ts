7de31167fd6f346f107b7f5935731b76e8026264
tntsus97 <89547932+tntsus97@users.noreply.github.com>
2022-04-26 16:24:35 -0600

    bing

--- Mobile Game/NoDice/src/app/tab1/tab1.page.ts
+++ Mobile Game/NoDice/src/app/tab1/tab1.page.ts
@@ -10,3 +10,17 @@ export class Tab1Page {
   constructor() {}
 
 }
+
+var d4 = document.getElementById('d4');
+
+d4.onclick;{
+var d4val = 4;
+};
+
+var d6 = document.getElementById('d6');
+
+d6.onclick;{
+  var d6val = 6;
+};
+
+var d8 = document.getElementById('d8');
