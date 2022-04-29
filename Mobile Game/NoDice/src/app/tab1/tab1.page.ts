2221e920d70559501b57aa2dbfa0561dd1a07487
tntsus97 <89547932+tntsus97@users.noreply.github.com>
2022-04-29 16:07:53 -0600

    dice history

--- Mobile Game/NoDice/src/app/tab1/tab1.page.ts
+++ Mobile Game/NoDice/src/app/tab1/tab1.page.ts
@@ -6,12 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public hist: Array<string> = []
   public async rollADice(faces){
     let randomNum = Math.floor(Math.random()*faces) + 1;
     console.log(randomNum);
     document.getElementById('diceResult').innerHTML = randomNum.toString();
-
-
+this.hist.push(randomNum.toString());
+localStorage.setItem('mydata',JSON.stringify(this.hist));
+console.log(this.hist);
   }
 
   public async rollCustom(customvalue){
@@ -19,6 +21,9 @@ export class Tab1Page {
     let customrandom = Math.floor(Math.random()*customvalue) + 1;
     console.log(customrandom);
     document.getElementById('diceResult').innerHTML = customrandom.toString();
+    this.hist.push(customrandom.toString());
+    localStorage.setItem('mydata',JSON.stringify(this.hist));
+console.log(this.hist);
   }
   constructor() {}
 
--- Mobile Game/NoDice/src/app/tab2/tab2.page.ts
+++ Mobile Game/NoDice/src/app/tab2/tab2.page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Tab1Page } from '../tab1/tab1.page';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+public async callhist(Tab1Page){
+let histo = JSON.parse(localStorage.getItem('mydata'));
+console.log(histo);
+document.getElementById('dicehistory').innerHTML = histo.toString();
+
+}
+
 
   constructor() {}
 
